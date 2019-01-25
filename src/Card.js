3812f48638bf29885d1ec11b8815dc5e6a30fce5
Munish Sharma <munish2870@hotmail.com>
2019-01-25 23:24:29 +0100

    Refactoring Card Component - added card list - better way to display cards with the help of map

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
-const card = (props) =>{
+const card = ({id, name, email}) =>{
 return(
     
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'> 
-    <img alt= 'Robots' src={`https://robohash.org/${props.id}?200x200`}/>
+    <img alt= 'Robots' src={`https://robohash.org/${id}?200x200`}/>
     <div> 
-    <h1>{props.name}</h1>
-    <p>{props.email}</p>
+    <h1>{name}</h1>
+    <p>{email}</p>
     </div>
     </div>
 )
