279d14e8024cbe61f679f9ef1f39aa889009e1f9
Munish Sharma <munish2870@hotmail.com>
2019-01-28 15:49:56 +0100

    Using Jsonplaceholder Api

--- src/App.js
+++ src/App.js
@@ -10,27 +10,35 @@ class App extends Component {
         super()
         this.state = {
 
-            robots: robots,
+            robots: [],
             searchField: ''
 
         }
     }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(users => this.setState({robots: users}));
+
+    }
     onSearchChange = (event) => {
-        this.setState({searchField : event.target.value})
+        this.setState({ searchField: event.target.value })
     }
     render() {
         const filterRobots =
             this.state.robots.filter(robots =>
                 robots.name.toLowerCase().includes(this.state.searchField.toLowerCase()))
-                
-    
-        return (
-            < div className='tc' >
-                <h1 className="pokemonFont f1"> RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filterRobots} />
-            </div >
-        )
+        if (this.state.robots.length === 0) {
+            return <h1 className="pokemonFont f1 tc">Loading...</h1>
+        } else {
+            return (
+                < div className='tc' >
+                    <h1 className="pokemonFont f1"> RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filterRobots} />
+                </div >
+            )
+        }
     }
 
 }
