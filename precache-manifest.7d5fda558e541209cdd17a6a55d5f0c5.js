bc2def2fb99cc0a96169c835cde3fc292e534f32
Munish Sharma <munish2870@hotmail.com>
2019-01-28 17:16:42 +0100

    Updates

---
+++ precache-manifest.7d5fda558e541209cdd17a6a55d5f0c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "510f8c1e1db5baf7478da8dbcf1044e3",
+    "url": "/RoboFriends/static/media/Pokemon Solid.510f8c1e.woff"
+  },
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "27f2354a1c97c477f941",
+    "url": "/RoboFriends/static/js/main.27f2354a.chunk.js"
+  },
+  {
+    "revision": "acb14efdbd839d651392",
+    "url": "/RoboFriends/static/js/1.acb14efd.chunk.js"
+  },
+  {
+    "revision": "27f2354a1c97c477f941",
+    "url": "/RoboFriends/static/css/main.b5e9af72.chunk.css"
+  },
+  {
+    "revision": "acb14efdbd839d651392",
+    "url": "/RoboFriends/static/css/1.190e7d97.chunk.css"
+  },
+  {
+    "revision": "35d84508cc0ff5fab7489a6ff4602554",
+    "url": "/RoboFriends/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.73c34ca4fb6ff5c9edfe6923c266da79.js"
+  "/RoboFriends/precache-manifest.7d5fda558e541209cdd17a6a55d5f0c5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/RoboFriends/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
