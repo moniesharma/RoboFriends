3561cebb31f472303b6a95e0396791824911439b
Munish Sharma <munish2870@hotmail.com>
2019-01-28 16:53:12 +0100

    Updates

---
+++ precache-manifest.41b94c742c5e98ec734ab0f6e0f50f69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "510f8c1e1db5baf7478da8dbcf1044e3",
+    "url": "/static/media/Pokemon Solid.510f8c1e.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "89e5a8d00bdaae52b458",
+    "url": "/static/js/main.89e5a8d0.chunk.js"
+  },
+  {
+    "revision": "acb14efdbd839d651392",
+    "url": "/static/js/1.acb14efd.chunk.js"
+  },
+  {
+    "revision": "89e5a8d00bdaae52b458",
+    "url": "/static/css/main.9630b9d3.chunk.css"
+  },
+  {
+    "revision": "acb14efdbd839d651392",
+    "url": "/static/css/1.190e7d97.chunk.css"
+  },
+  {
+    "revision": "999bfd1192a94683df0b9b141228027c",
+    "url": "/index.html"
+  }
+];
